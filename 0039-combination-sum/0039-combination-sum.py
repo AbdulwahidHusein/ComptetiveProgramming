dc4b439f1571a0ffadccc5820b92b9ae6c5f97ab
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2023-10-31 01:49:26 -0700

    Time: 64 ms (46.43%), Space: 16.4 MB (33.08%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.ans = []
+        def traverse(candid, arr,sm):               
+            if sm == target: 
+                self.ans.append(arr)
+                return
+            if sm >= target: 
+                return   
+            for i in range(len(candid)):
+                traverse(candid[i:], arr + [candid[i]], sm+candid[i])
+        traverse(candidates,[], 0)
+        return self.ans
