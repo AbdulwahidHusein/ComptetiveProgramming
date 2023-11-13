449e117dcf11f0ff9f83ab033dd3421bf9f37f0d
Abdulwahid Hussen <134911626+AbdulwahidHusein@users.noreply.github.com>
2023-11-13 10:18:30 -0800

    Time: 95 ms (93.61%), Space: 16.1 MB (70.1%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        MAX = int(c**0.5)
+        smaller = 0
+        larger = MAX
+        while smaller <= larger:
+            product = smaller*smaller + larger*larger
+            if product == c:
+                return True
+            elif product > c:
+                larger -= 1
+            else:
+                smaller += 1
+        return False
